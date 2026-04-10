9589f74f184108515b72bccd2ad5db593cbae347
Ana B <budimirovicana4@gmail.com>
2026-04-11 00:15:57 +0200

    login activity

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/MainActivity.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.eventsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button firstLogin, secondLogin, firstRegister, secondRegister;
+    private EditText loginUsername, loginPassword;
+    private EditText registerUsername, registerPassword, registerEmail;
+    private TextView tvUsername, tvEmail, tvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstLogin = findViewById(R.id.firstLogin);
+        secondLogin = findViewById(R.id.secondLogin);
+        firstRegister = findViewById(R.id.firstRegister);
+        secondRegister = findViewById(R.id.secondRegister);
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPassword = findViewById(R.id.loginPassword);
+        registerUsername = findViewById(R.id.registerUsername);
+        registerPassword = findViewById(R.id.registerPassword);
+        registerEmail = findViewById(R.id.registerEmail);
+        tvUsername = findViewById(R.id.textViewUsername);
+        tvPassword = findViewById(R.id.textViewPassword);
+        tvEmail = findViewById(R.id.textViewEmail);
+
+//        loginUsername.setVisibility(View.GONE);
+//        loginPassword.setVisibility(View.GONE);
+//        secondRegister.setVisibility(View.GONE);
+//        secondLogin.setVisibility(View.GONE);
+//        registerEmail.setVisibility(View.GONE);
+//        registerPassword.setVisibility(View.GONE);
+//        registerUsername.setVisibility(View.GONE);
+//        tvEmail.setVisibility(View.GONE);
+//        tvUsername.setVisibility(View.GONE);
+//        tvPassword.setVisibility(View.GONE);
+
+        firstLogin.setOnClickListener(this);
+        secondLogin.setOnClickListener(this);
+        firstRegister.setOnClickListener(this);
+        secondRegister.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.firstLogin){
+            firstRegister.setVisibility(View.GONE);
+            firstLogin.setVisibility(View.GONE);
+            loginUsername.setVisibility(View.VISIBLE);
+            loginPassword.setVisibility(View.VISIBLE);
+            secondLogin.setVisibility(View.VISIBLE);
+        }
+
+        else if(view.getId() == R.id.firstRegister){
+            firstRegister.setVisibility(View.GONE);
+            firstLogin.setVisibility(View.GONE);
+            secondRegister.setVisibility(View.VISIBLE);
+            registerEmail.setVisibility(View.VISIBLE);
+            registerPassword.setVisibility(View.VISIBLE);
+            registerUsername.setVisibility(View.VISIBLE);
+            tvEmail.setVisibility(View.VISIBLE);
+            tvUsername.setVisibility(View.VISIBLE);
+            tvPassword.setVisibility(View.VISIBLE);
+        }
+        else if(view.getId() == R.id.secondLogin){
+            String username = loginUsername.getText().toString();
+            String password = loginPassword.getText().toString();
+
+            if(username.equals("admin") && password.equals("admin")){
+                //prelazak na sledeci activity
+                Intent intent = new Intent(MainActivity.this,
+                        EventsActivity.class);
+                startActivity(intent);
+            }
+        }
+        else if(view.getId() == R.id.secondRegister){
+            String username = registerUsername.getText().toString();
+            String password = registerPassword.getText().toString();
+            String email = registerEmail.getText().toString();
+
+            if(username.equals("admin") && password.equals("admin") && email.equals("admin@gmail.com")){
+                //prelazak na sledeci activity
+                Intent intent = new Intent(MainActivity.this,
+                        EventsActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
