c3d0fd8f26a141eca03336d475c9d89a6c66caab
Ana B <budimirovicana4@gmail.com>
2026-04-11 11:52:33 +0200

    events activity started

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventsActivity.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventsActivity.java
@@ -3,8 +3,14 @@ package com.example.eventsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class EventsActivity extends AppCompatActivity {
+public class EventsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvWelcome,tvUsername, tvUpcominEvents;
+    private Button events, myEvents, friends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,22 @@ public class EventsActivity extends AppCompatActivity {
         String username = bundle.getString("username");
         String email = bundle.getString("email");  //ako je prosledjen samo username email ce biti null
 
+        tvUsername = findViewById(R.id.username);
+        tvUsername.setText(username);
+        tvWelcome = findViewById(R.id.welcome);
+        tvUpcominEvents = findViewById(R.id.tvUpcomingEvents);
 
+        events = findViewById(R.id.events);
+        myEvents = findViewById(R.id.myEvents);
+        friends = findViewById(R.id.friends);
+        friends.setEnabled(false);
 
+        events.setOnClickListener(this);
+        myEvents.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
 
     }
 }
