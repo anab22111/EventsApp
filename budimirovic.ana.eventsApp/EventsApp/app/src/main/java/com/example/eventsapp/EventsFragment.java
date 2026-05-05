f0d0943e790e9aab7ce7bcc943f7500af940a2a2
Ana B <budimirovicana4@gmail.com>
2026-05-05 23:14:29 +0200

    onViewCreated() added

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventsFragment.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventsFragment.java
@@ -2,12 +2,16 @@ package com.example.eventsapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 ///**
 // * A simple {@link Fragment} subclass.
@@ -22,6 +26,17 @@ public class EventsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        // just findViewById doesnt work because the fragment doesnt have that method, therefore need to use view
+        ListView list = view.findViewById(R.id.list);
+        TextView emptyView = view.findViewById(R.id.tvNoUpcomingEvents);
+
+        // set empty view of list to tvNoUpcomingEvents
+        list.setEmptyView(emptyView);
     }
 
 
