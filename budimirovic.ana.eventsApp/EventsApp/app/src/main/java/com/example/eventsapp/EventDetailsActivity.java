8456feebf3390e84d47c13ee2231410348defb32
Ana B <budimirovicana4@gmail.com>
2026-05-07 23:22:39 +0200

    onCLick for buttons finished

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventDetailsActivity.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/EventDetailsActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EventDetailsActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView tvName, tvCategory, tvDescription, tvLocation, tvDateTime, tvRating, tvFreeSeats;
     private Button btnInterested, btnAttending;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class EventDetailsActivity extends AppCompatActivity implements View.OnCl
         tvFreeSeats = findViewById(R.id.tvFreeSeats);
         btnInterested = findViewById(R.id.btnInterested);
         btnAttending = findViewById(R.id.btnAttending);
+        image = findViewById(R.id.image);
 
         tvName.setText(event.getName());
         tvCategory.setText(event.getCategory());
@@ -36,6 +40,7 @@ public class EventDetailsActivity extends AppCompatActivity implements View.OnCl
         tvLocation.setText(event.getLocation());
         tvRating.setText("No rating yet");
         tvDateTime.setText(event.getDateTime());
+        image.setImageResource(R.drawable.marathon_runner);
 
         if(event.isPromoted()){
             tvFreeSeats.setText("Slobodnih mesta: " + event.getCapacity()+"/"+event.getCapacity());
@@ -47,12 +52,18 @@ public class EventDetailsActivity extends AppCompatActivity implements View.OnCl
         btnInterested.setOnClickListener(this);
         btnAttending.setOnClickListener(this);
 
-
-
     }
 
     @Override
     public void onClick(View view) {
+
+        if(view.getId() == R.id.btnInterested){
+            Toast.makeText(EventDetailsActivity.this, "Added to interested.", Toast.LENGTH_SHORT).show();
+
+        }else if(view.getId() == R.id.btnAttending){
+            Toast.makeText(EventDetailsActivity.this, "You have registered for the event.", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 }
