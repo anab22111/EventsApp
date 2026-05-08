d12fa576d3862c17db171112c8370f4fe8b302b0
Ana B <budimirovicana4@gmail.com>
2026-05-08 15:27:57 +0200

    rating_event xml done

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/RatingActivity.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/RatingActivity.java
@@ -3,12 +3,27 @@ package com.example.eventsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class RatingActivity extends AppCompatActivity {
+    private TextView tvRateEvent, tvName;
+    private Button btnConfirm;
+    private CheckBox star1, star2, star3, star4, star5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        // get name of event
+        String name = getIntent().getStringExtra("nameOfEvent");
+
+        tvRateEvent = findViewById(R.id.tvRate);
+        tvRateEvent.setText(name);   // set name of event
+        
+
+
     }
 }
