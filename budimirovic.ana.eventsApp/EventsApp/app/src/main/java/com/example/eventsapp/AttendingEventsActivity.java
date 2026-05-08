c1f962180c70f6345069e2690ae752b5383438fc
Ana B <budimirovicana4@gmail.com>
2026-05-08 14:19:30 +0200

    attendingEvents complete

--- budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/AttendingEventsActivity.java
+++ budimirovic.ana.eventsApp/EventsApp/app/src/main/java/com/example/eventsapp/AttendingEventsActivity.java
@@ -2,8 +2,12 @@ package com.example.eventsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,11 +15,9 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class AttendingEventsActivity extends AppCompatActivity {
+public class AttendingEventsActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView tvHeaderUpcoming, tvHeaderPast, tvNoEvents;
-    private ListView lvPast, lvUpcoming;
-    private EventAdapter adapterUpcoming, adapterPast;
-    private ArrayList<Event> listUpcoming, listPast;
+    private LinearLayout containerPast, containerUpcoming;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,39 +26,81 @@ public class AttendingEventsActivity extends AppCompatActivity {
 
         tvHeaderPast = findViewById(R.id.tvHeaderPast);
         tvHeaderUpcoming = findViewById(R.id.tvHeaderUpcoming);
-        lvPast = findViewById(R.id.listPassed);
-        lvUpcoming = findViewById(R.id.listUpcoming);
         tvNoEvents = findViewById(R.id.tvNoAttendEvents);
-        listUpcoming = new ArrayList<>();
-        listPast = new ArrayList<>();
+        containerPast = findViewById(R.id.containerPassed);
+        containerUpcoming = findViewById(R.id.containerUpcoming);
 
+        getAppearance();
+    }
 
-        // get lists from AppData
-        for(Event e : AppData.attendingEvents){    // go through attendingEvents list
+    public void getAppearance(){
+        // refresh everything
+        containerUpcoming.removeAllViews();
+        containerPast.removeAllViews();
 
-            // check if the event pasesed
-            if(e.isPast()){  // if it passed add to listPast
-                listPast.add(e);
-            }else{
-                listUpcoming.add(e);  // else add to listUpcoming
+        boolean hasUpcoming = false;
+        boolean hasPast = false;
+
+        // create adapter for attendingEvents list
+        EventAdapter adapter = new EventAdapter(this, (ArrayList<Event>) AppData.attendingEvents);
+
+        // go through list and separate passed and upcoming events
+        for (int i = 0; i < AppData.attendingEvents.size(); i++) {
+            Event e = AppData.attendingEvents.get(i);   // get event with index i
+
+            // get View for one row
+            View row = adapter.getView(i, null, null);   // "make" view for an event from attendingEvents on index i
+
+            // hide image, featured and color, category, freeSeats
+            row.findViewById(R.id.eventImage).setVisibility(View.GONE);
+            row.findViewById(R.id.tvPromoted).setVisibility(View.GONE);
+            row.setBackgroundResource(android.R.color.white);
+            row.findViewById(R.id.tvCategory).setVisibility(View.GONE);
+            row.findViewById(R.id.tvFreeSeats).setVisibility(View.GONE);
+
+            // check to see if event passed or not
+            if (e.isPast()) {
+                hasPast = true;
+                containerPast.addView(row);    // add view to linearLayout
+
+                Button btnRate = row.findViewById(R.id.btnRate);    // get button from row View
+                if (btnRate != null) {                              // set to visible
+                    btnRate.setVisibility(View.VISIBLE);
+                    btnRate.setTag(e.getName());                    // set tag to transfer name - tag is connected to button
+                    btnRate.setOnClickListener(this);               // set listener
+                }
+
+            }
+
+            else {
+                hasUpcoming = true;
+                containerUpcoming.addView(row);
+
+                Button btnRate = row.findViewById(R.id.btnRate);
+                if (btnRate != null) {
+                    btnRate.setVisibility(View.GONE);   // when upcoming event hide rate button
+                }
             }
         }
 
-        // make adapters
-        adapterPast = new EventAdapter(this, listPast);
-        adapterUpcoming = new EventAdapter(this, listUpcoming);
 
-        lvPast.setAdapter(adapterPast);
-        lvUpcoming.setAdapter(adapterUpcoming);
+        tvHeaderUpcoming.setVisibility(hasUpcoming ? View.VISIBLE : View.GONE);
+        tvHeaderPast.setVisibility(hasPast ? View.VISIBLE : View.GONE);
+        tvNoEvents.setVisibility(AppData.attendingEvents.isEmpty() ? View.VISIBLE : View.GONE);
+    }
 
-        // check if lists are empty
-        if(listUpcoming.isEmpty()){tvHeaderUpcoming.setVisibility(View.GONE);}  // hide header
-        if(listPast.isEmpty()){tvHeaderPast.setVisibility(View.GONE);}   // hide header
-        if(listUpcoming.isEmpty() && listPast.isEmpty()){
-            tvNoEvents.setVisibility(View.VISIBLE);
-        }else{
-            tvNoEvents.setVisibility(View.GONE);
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnRate) {
+
+            // from tag get name
+            String eventName = (String) view.getTag();
+
+            Intent intent = new Intent(this, RatingActivity.class);
+            intent.putExtra("nameOfEvent", eventName);
+            startActivity(intent);
         }
 
     }
+
 }
